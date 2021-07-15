b1c6dfa4585d88a36bd3649894267e4529fb992f
is0xjh25 <is0.jh25@gmail.com>
2021-07-15 20:35:01 +1000

    15.Three Sum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,6 +1,5 @@
 /*
-15.3Sum
-
+15.Three Sum
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
